af7c357d1e840a0744a6ce912298bf5a2054f756
Alexander <alexandergsell997@gmail.com>
2024-05-10 20:06:25 +0200

    Continue

--- server/workers/configConnexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-	//Informations de connexion à la base de données
-	define('DB_TYPE', 'mysql');
-    define('DB_HOST', 'gsella.emf-informatique.ch');
-    define('DB_NAME', 'gsella_151');
-    define('DB_USER', 'gsella_root');
-    define('DB_PASS', 'rr1W$WGP&(Nq');
-?>
